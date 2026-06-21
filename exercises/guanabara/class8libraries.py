c977d4015ad5c7614f0c3f9b50da51b7ac5cf735
Lysouto <gamernoob847@gmail.com>
2026-06-21 15:34:41 -0300

    added emoji library

--- exercises/guanabara/class8libraries.py
+++ exercises/guanabara/class8libraries.py
@@ -4,3 +4,6 @@ num = int(input("Type a number: "))
 root = ceil(sqrt(num))
 print(f"the square root of {num} is {root}.")
 
+import emoji
+
+print(emoji.emojize("testing emoji library :cold_face:"))
