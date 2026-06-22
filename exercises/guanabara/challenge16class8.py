25c2533dffe1dfeecab82e64f13b01c13e8cace4
Lysouto <gamernoob847@gmail.com>
2026-06-22 19:47:18 -0300

    Solved exercise 16

--- exercises/guanabara/challenge16class8.py
+++ exercises/guanabara/challenge16class8.py
@@ -1,2 +1,10 @@
 """Create a program that reads 
 any Real number and shows its integer part."""
+
+from math import trunc
+
+
+
+real_num = float(input("Type a real number: "))
+integer = trunc(real_num)
+print(f"The integer part of {real_num}, is {integer}")
