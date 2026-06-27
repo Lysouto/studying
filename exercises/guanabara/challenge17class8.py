02a79be018f8c1d0453b937e62e986b4ce8fd42a
Lysouto <gamernoob847@gmail.com>
2026-06-27 20:42:25 -0300

    Solved exercise

--- exercises/guanabara/challenge17class8.py
+++ exercises/guanabara/challenge17class8.py
@@ -1,3 +1,15 @@
 """Create a program that reads the size of the opposite and 
-the adjsacent side of a right triangle, calculates
+the adjacent side of a right triangle, calculates
 and shows the size of the hypotenuse."""
+
+import math
+
+opposite_side = float(input("Type the value of the" \
+" opposite side: "))
+adjacent_side = float(input("Type the value of the " \
+"adjacent side: "))
+
+sumofthesquare = math.pow(opposite_side,2) + math.pow(adjacent_side,2)
+hypotenuse = math.sqrt(sumofthesquare)
+
+print(f"The hypostenuse of the triangle is {hypotenuse:.2f}.")
